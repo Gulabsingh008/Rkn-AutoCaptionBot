9510930259929076894487187716274a64f3bf06
Amarsingh008 <amarsinghrj92@gmail.com>
2024-12-01 05:27:57 -0800

    Update config.py

--- config.py
+++ config.py
@@ -36,10 +36,10 @@ class Rkn_Bots(object):
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
     # admin id  ( required.. 😥)
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7170452349').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7170452349 5346278816').split()]
     
 
 # Rkn Developer 
-# Don't Remove Credit 😔
+# Don't Remove Credit 😔 
 # Telegram Channel @RknDeveloper & @Rkn_Botz
 # Developer @RknDeveloperr
